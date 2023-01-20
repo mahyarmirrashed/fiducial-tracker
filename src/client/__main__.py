f4540b8ea8792baf786fb14109c3c6ea1c646161
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-20 04:12:40 -0600

    feat: add random uniform message generation

--- src/client/__main__.py
+++ src/client/__main__.py
@@ -1,2 +1,29 @@
 #!/home/mmirrashed/.conda/envs/tracker/bin/python
-print("Hello from client!")
+from argparse import ArgumentParser
+from src.helpers import IntegerRange
+
+import random
+import time
+import zmq
+
+DEFAULT_COLLECTOR_PORT = 5000
+
+parser = ArgumentParser(description="Fiducial tracker client.")
+
+parser.add_argument(
+  "-p",
+  "--port",
+  type=IntegerRange(1024, 65536),
+  default=DEFAULT_COLLECTOR_PORT,
+  help="Port number for publishing captured video stream",
+)
+
+args = parser.parse_args()
+
+context = zmq.Context()
+publisher = context.socket(zmq.PUB)
+publisher.connect(f"tcp://localhost:{args.port}")
+
+while True:
+  publisher.send_string("Ping!")
+  time.sleep(random.random())
