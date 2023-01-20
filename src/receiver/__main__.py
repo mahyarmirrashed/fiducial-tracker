7369ca6a9280a5d0c6d89dc6cff0405b2a691a61
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-20 03:19:38 -0600

    feat: add pub/sub messaging from server to receivers
    Add a basic layout of what pub/sub messaging would behave in ZMQ.

--- src/receiver/__main__.py
+++ src/receiver/__main__.py
@@ -1,2 +1,39 @@
 #!/home/mmirrashed/.conda/envs/tracker/bin/python
-print("Hello from receiver!")
+from argparse import ArgumentParser
+from src.helpers import IntegerRange
+
+import time
+import zmq
+
+DEFAULT_SERVER_PORT = 5000
+
+# python -m src.receiver -p PORT
+parser = ArgumentParser(description="Fiducial tracker receiver.")
+
+parser.add_argument(
+  "-p",
+  "--port",
+  type=IntegerRange(1024, 65536),
+  default=DEFAULT_SERVER_PORT,
+  help="Server port number",
+)
+
+args = parser.parse_args()
+
+context = zmq.Context()
+subscriber = context.socket(zmq.SUB)
+subscriber.connect(f"tcp://localhost:{args.port}")
+subscriber.setsockopt(zmq.SUBSCRIBE, b"")
+
+count = 0
+avg = 0
+
+while True:
+  start = time.time()
+  res = subscriber.recv_string()
+  end = time.time()
+  avg *= count
+  avg += (end - start)
+  count += 1
+  avg /= count
+  print(f"{res}, avg res time: {avg:.3f}")
--- src/server/__main__.py
+++ src/server/__main__.py
@@ -1,2 +1,30 @@
 #!/home/mmirrashed/.conda/envs/tracker/bin/python
-print("Hello from server!")
+from argparse import ArgumentParser
+from src.helpers import IntegerRange
+
+import random
+import time
+import zmq
+
+DEFAULT_SERVER_PORT = 5000
+
+# python -m src.server -p PORT
+parser = ArgumentParser(description="Fiducial tracker server.")
+
+parser.add_argument(
+  "-p",
+  "--port",
+  type=IntegerRange(1024, 65536),
+  default=DEFAULT_SERVER_PORT,
+  help="Server port number",
+)
+
+args = parser.parse_args()
+
+context = zmq.Context()
+publisher = context.socket(zmq.PUB)
+publisher.bind(f"tcp://*:{args.port}")
+
+while True:
+  publisher.send_string("Ping!")
+  time.sleep(random.random())
