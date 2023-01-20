6368668772ee4035225715024eb46c7e71bb7592
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-20 03:51:30 -0600

    refactor: rename cli args to accommodate for multiple port number args

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -6,24 +6,31 @@ import random
 import time
 import zmq
 
-DEFAULT_SERVER_PORT = 5000
+DEFAULT_COLLECTOR_PORT = 5000
+DEFAULT_PUBLISHER_PORT = 6000
 
-# python -m src.server -p PORT
-parser = ArgumentParser(description="Fiducial tracker server.")
+parser = ArgumentParser(description="Fiducial tracking server.")
 
 parser.add_argument(
-  "-p",
-  "--port",
+  "--collector",
   type=IntegerRange(1024, 65536),
-  default=DEFAULT_SERVER_PORT,
-  help="Server port number",
+  default=DEFAULT_COLLECTOR_PORT,
+  help="Port number for collecting video streams",
+)
+parser.add_argument(
+  "--publisher",
+  type=IntegerRange(1024, 65536),
+  default=DEFAULT_PUBLISHER_PORT,
+  help="Port number for publishing tracked fiducial locations",
 )
 
 args = parser.parse_args()
 
 context = zmq.Context()
+collector = context.socket(zmq.SUB)
+collector.bind(f"tcp://*:{args.collector}")
 publisher = context.socket(zmq.PUB)
-publisher.bind(f"tcp://*:{args.port}")
+publisher.bind(f"tcp://*:{args.publisher}")
 
 while True:
   publisher.send_string("Ping!")
