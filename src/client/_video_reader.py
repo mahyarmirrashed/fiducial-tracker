5c47e7f9fd17507ba7c97fc93ffb213bae44f228
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-24 17:33:07 -0600

    feat: add wrapper class for managing resource usage

---
+++ src/client/_video_reader.py
@@ -0,0 +1,26 @@
+from types import TracebackType
+from typing import Generator, Union
+from typing_extensions import Self
+
+import cv2 as cv
+import numpy as np
+
+
+class VideoReader:
+  def __init__(self, source: Union[int, str]) -> None:
+    """Wrapper for OpenCV video reading interactions."""
+    self._cap = cv.VideoCapture(source)
+
+    if self._cap is None or not self._cap.isOpened():
+      raise ValueError(f"Unable to open video source: {source}")
+
+  def __enter__(self) -> Self:
+    return self
+
+  def __exit__(
+    self,
+    exc_type: Union[BaseException, None],
+    exc_value: Union[BaseException, None],
+    traceback: Union[TracebackType, None],
+  ) -> None:
+    self._cap.release()
