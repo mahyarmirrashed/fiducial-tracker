e79c620a0fef152bbecb4a64b706158475a92f74
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-27 11:44:35 -0600

    fix: prevent extra, unspecified fields onto class

--- src/common/models/_base.py
+++ src/common/models/_base.py
@@ -2,13 +2,14 @@
 
 
 from orjson import dumps, loads
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra
 
 
 class _FrozenModel(BaseModel):
   """Frozen model configured with orjson extensions."""
 
   class Config:
+    extra = Extra.forbid
     frozen = True
     json_loads = loads
     json_dumps = lambda v, *, default: dumps(v, default=default).decode()
