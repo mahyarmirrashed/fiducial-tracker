d8979c567852ece54860414069f97e7a07786db3
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-27 20:27:37 -0600

    feat: expose needed classes to outside submodules

--- src/common/models/__init__.py
+++ src/common/models/__init__.py
@@ -1,1 +1,2 @@
-from ._base import BaseMessage
+from .stream_location import LocationStreamRequestMessage, LocationStreamResponseMessage
+from .stream_video import VideoStreamRequestMessage, VideoStreamResponseMessage
