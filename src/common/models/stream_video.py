4916ddc08773e93e46164c175361e1655e40ebfd
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-29 01:07:00 -0600

    feat: allow sending frames as numpy arrays

--- src/common/models/stream_video.py
+++ src/common/models/stream_video.py
@@ -3,18 +3,19 @@
 from typing import Optional
 
 import datetime
-import numpy as np
 import uuid
 
 from ._base import BaseMessage
 from ._point import Point
+
+from src.common.models.custom import NDArrayUint8
 
 
 class VideoStreamRequestMessage(BaseMessage):
   """Implements the VideoStream request message arguments."""
 
   client_id: uuid.UUID
-  frame: np.ndarray
+  frame: NDArrayUint8
   timestamp: datetime.datetime
   corner_bottom_right: Optional[Point]
   corner_upper_left: Optional[Point]
