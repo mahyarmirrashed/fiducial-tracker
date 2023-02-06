7539c8bef30ae7abae9fcb74cf673625782303ed
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-02-06 02:47:16 -0600

    refactor: remove `orjson` as a dependency

--- src/common/models/_frozen_model.py
+++ src/common/models/_frozen_model.py
@@ -1,13 +1,6 @@
 """Defines a frozen model with JSON encoder configurations."""
 
-from orjson import dumps as _dumps, loads
 from pydantic import BaseModel, Extra
-
-import orjson
-
-
-def dumps(v, *, default):
-  return _dumps(v, default=default, option=orjson.OPT_SERIALIZE_NUMPY).decode()
 
 
 class FrozenModel(BaseModel):
@@ -16,5 +9,3 @@ class FrozenModel(BaseModel):
   class Config:
     extra = Extra.forbid
     frozen = True
-    json_loads = loads
-    json_dumps = dumps
