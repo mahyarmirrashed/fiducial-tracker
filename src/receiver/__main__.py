9a0afbc829ba02cedfe7f16ce23e123231b8c101
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-02-06 17:51:17 -0600

    feat(receiver): add ability to receive location stream requests

--- src/receiver/__main__.py
+++ src/receiver/__main__.py
@@ -1,24 +1,12 @@
 #!/home/mmirrashed/.conda/envs/tracker/bin/python
+from src.common import display
+
 from ._args import args
+from ._comms import Communicator
 
-import time
-import zmq
-
-
-context = zmq.Context()
-receiver = context.socket(zmq.SUB)
-receiver.connect(f"tcp://localhost:{args.port}")
-receiver.setsockopt(zmq.SUBSCRIBE, b"")
-
-count = 0
-avg = 0
-
-while True:
-  start = time.time()
-  res = receiver.recv_string()
-  end = time.time()
-  avg *= count
-  avg += end - start
-  count += 1
-  avg /= count
-  print(f"{res}, avg res time: {avg:.3f}")
+try:
+  with Communicator(args.port) as comms:
+    while req := comms.recv():
+      pass
+except KeyboardInterrupt:
+  display("Receiver is exiting...")
---
+++ src/receiver/_comms.py
@@ -0,0 +1,37 @@
+from pydantic import NonNegativeInt
+from types import TracebackType
+from typing import Optional, Union
+from typing_extensions import Self
+
+from src.common.models import LocationStreamRequestMessage
+
+import ormsgpack
+import zmq
+
+
+class Communicator:
+  def __init__(self, port: NonNegativeInt) -> None:
+    """Communicator with fiducial tracker server."""
+    self._port = port
+
+  def __enter__(self) -> Self:
+    self._context = zmq.Context()
+    self._socket = self._context.socket(zmq.SUB)
+    self._socket.connect(f"tcp://localhost:{self._port}")
+    self._socket.setsockopt(zmq.SUBSCRIBE, b"")
+
+    return self
+
+  def __exit__(
+    self,
+    exc_type: Union[BaseException, None],
+    exc_value: Union[BaseException, None],
+    traceback: Union[TracebackType, None],
+  ) -> None:
+    self._socket.close()
+    self._context.term()
+
+  def recv(self) -> Optional[LocationStreamRequestMessage]:
+    if (msg := self._socket.recv()) is not None:
+      return LocationStreamRequestMessage(**ormsgpack.unpackb(msg))
+    return None
