4980f81cb95179672976adf833bd2c54478fd187
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-02-07 01:15:30 -0600

    feat: make point type return a `Point` model

--- src/common/point_type.py
+++ src/common/point_type.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentTypeError
+from pydantic import NonNegativeInt
 from typing import Any, Tuple
+
+from src.common.models import Point
 
 
 class PointType:
   def __init__(self) -> None:
     pass
 
-  def __call__(self, arg: Any) -> Tuple[float, float]:
+  def __call__(self, arg: Any) -> Point:
     try:
       x, y = map(float, arg.split(","))
     except:
       raise ArgumentTypeError('Coordinates must be in the "x,y" form')
     else:
-      return x, y
+      return Point(x=NonNegativeInt(x), y=NonNegativeInt(y))
