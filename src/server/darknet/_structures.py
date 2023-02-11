97500135109c645dffc24f2742df5e556d775912
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-02-10 20:55:40 -0600

    feat: add object detection structure

--- src/server/darknet/_structures.py
+++ src/server/darknet/_structures.py
@@ -1,4 +1,4 @@
-from ctypes import c_float, Structure
+from ctypes import c_float, c_int, POINTER, Structure
 
 
 class _BoxStruct(Structure):
@@ -7,4 +7,22 @@ class _BoxStruct(Structure):
     ("y", c_float),
     ("w", c_float),
     ("h", c_float),
+  ]
+
+
+class _DetectionStruct(Structure):
+  _fields_ = [
+    ("bbox", _BoxStruct),
+    ("classes", c_int),
+    ("best_class_idx", c_int),
+    ("prob", POINTER(c_float)),
+    ("mask", POINTER(c_float)),
+    ("objectness", c_float),
+    ("sort_class", c_int),
+    ("uc", POINTER(c_float)),
+    ("points", c_int),
+    ("embeddings", POINTER(c_float)),
+    ("embedding_size", c_int),
+    ("sim", c_float),
+    ("track_id", c_int),
   ]
