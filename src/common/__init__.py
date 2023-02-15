3e54408201770ece5e4f32a670b3a98310120c76
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-02-14 18:31:24 -0600

    fix: make float range type visible to other modules

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,4 +1,5 @@
 from .clear import clear, display
 from .file_type import FileType
+from .float_range_type import FloatRangeType
 from .integer_range_type import IntegerRangeType
 from .point_type import PointType
