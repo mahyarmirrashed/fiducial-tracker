3e01e1c2eb8c12e646de0c4f6e5e925b0552169c
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-03-02 21:11:15 -0600

    fix: add logger factory as a module export

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -3,4 +3,5 @@ from ._file_type import FileType
 from ._float_range_type import FloatRangeType
 from ._heartbeat import Heartbeat
 from ._integer_range_type import IntegerRangeType
+from ._logger_factory import logger_factory
 from ._point_type import PointType
