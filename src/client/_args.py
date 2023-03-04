a19bf844d4a08ff2b79f59ca316ee712b87ec1f0
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-03-03 22:55:13 -0600

    feat(client): add argument for supplying firebase rtdb certificate file

--- src/client/_args.py
+++ src/client/_args.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from src.common import SocketAddressType
+from src.common import FileType, SocketAddressType
 from src.common.models import SocketAddress
 
 _DEFAULT_LOCATION_STREAM_ADDRESS = SocketAddress(host="localhost", port=6000)
@@ -13,6 +13,11 @@ _parser.add_argument(
   type=SocketAddressType(),
   default=_DEFAULT_LOCATION_STREAM_ADDRESS,
   help="Socket address for collecting tracked fiducial locations",
+)
+_parser.add_argument(
+  "--firebase-certificate",
+  type=FileType("json"),
+  help="Path to Firebase database certificate file (.json)",
 )
 
 args = _parser.parse_args()
