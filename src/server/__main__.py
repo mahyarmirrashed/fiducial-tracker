be126958fef5f46bed91f4a29b8d3ecf0c1548fe
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-03-08 00:15:43 -0600

    feat(server): add exit condition to discard stale requests and reply with default fps recommendation

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -1,4 +1,5 @@
 #!/home/mmirrashed/.conda/envs/tracker/bin/python
+import datetime
 import time
 
 import cv2 as cv
@@ -14,6 +15,9 @@ from ._diagnostics import show_status
 from ._display import display_manager
 from ._logger import logger
 
+_ALLOWED_DELAY = 5.0  # seconds
+_DEFAULT_FPS_RECOMMENDATION = 30.0
+
 _camera_feed_cache = TTLCache(maxsize=100, ttl=10)
 _camera_feed_window_manager_heartbeat = Heartbeat(5)
 _fiducial_cache = TTLCache(maxsize=10, ttl=5)
@@ -26,6 +30,12 @@ show_status(running=True)
 try:
   with Communicator(args.location_stream_address, args.video_stream_address) as comms:
     while req := comms.recv_video_stream():
+      if req.timestamp + _ALLOWED_DELAY < time.time():
+        logger.debug("Received request is stale and was discarded.")
+
+        comms.send_video_stream(_DEFAULT_FPS_RECOMMENDATION)
+        continue  # skip rest of loop
+
       # log connected camera uuid
       _camera_feed_cache[req.camera_id] = None
 
